25e71b9ed60375a73249c0a43003a6bfb79535ad
Kamaleshvar <109738299+Kamaleshvar1@users.noreply.github.com>
2024-10-25 14:51:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for
 import sqlite3
 from datetime import datetime
-import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
 
 
 app = Flask(__name__)
